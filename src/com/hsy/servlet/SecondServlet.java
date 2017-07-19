985166de3558a109cbc250e48025518377fa9292
huangsongyan <huberyhuang52@gmail.com>
2017-07-19 17:53:03 +0800

    first commit

---
+++ src/com/hsy/servlet/SecondServlet.java
@@ -0,0 +1,22 @@
+package com.hsy.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by hsy on 2017/7/19.
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "SecondServlet" + "</h1>");
+    }
+}
---
+++ src/com/hsy/servlet/TestServlet.java
@@ -0,0 +1,38 @@
+package com.hsy.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by hsy on 2017/7/19.
+ */
+public class TestServlet extends HttpServlet {
+
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello world, this message is from servlet!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      //  super.doGet(req, resp);
+        resp.setContentType("text/html");
+
+        //设置逻辑实现
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+
+}
